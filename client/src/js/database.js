eb2594004e8b5273b0a4874464edd1bcea398792
alexoserna <118146045+alexoserna@users.noreply.github.com>
2023-05-19 14:54:43 -0400

    made adjustments in order to work correctly

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,31 +14,26 @@ const initdb = async () =>
 
 // Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  try {
-    const db = await openDB('jate', 1);
-    const tx = db.transaction('jate', 'readwrite');
-    const store = tx.objectStore('jate');
-    await store.add({ content });
-    await tx.complete;
-    console.log('Content added to the database');
-  } catch (error) {
-    console.error('Error adding content to the database:', error);
-  }
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('Data saved to the database', result.value);
 };
 
 // Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  try {
-    const db = await openDB('jate', 1);
-    const tx = db.transaction('jate', 'readonly');
-    const store = tx.objectStore('jate');
-    const content = await store.getAll();
-    console.log('Retrieved content from the database:', content);
-    return content;
-  } catch (error) {
-    console.error('Error retrieving content from the database:', error);
-    return [];
-  }
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const content = await store.get(1);
+  const result = await content;
+  result
+      ? console.log('🚀 - data retrieved from the database', result.value)
+      : console.log('🚀 - data not found in the database');
+
+  return result?.value;
 };
 
 initdb();
